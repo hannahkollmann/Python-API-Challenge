07ffeaf3fdf3751e5b96a6fd52c814b7d6368486
Hannah Kollmann <157349641+hannahkollmann@users.noreply.github.com>
2024-04-09 22:44:12 -0500

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "24d5032944414f7f606cb24708652d34"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "4e7508f96038417090a45c530cb0c0e0"
